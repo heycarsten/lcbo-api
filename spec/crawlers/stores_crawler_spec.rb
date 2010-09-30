f78496d4168c28d5f42a416a095fd1d3005f07d0
Carsten Nielsen <heycarsten@gmail.com>
2010-09-30 00:04:21 -0400

    Removing cruft.

--- spec/crawlers/stores_crawler_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe StoresCrawler do
-
-  it 'should be tested'
-
-end
--- spec/jobs/parse_crawl_response_job_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe ParseCrawlResponseJob do
-  it 'should be tested'
-end
