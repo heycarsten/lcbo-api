b1497a6f8b2fcfa174c43f44c1f9e9811c00077d
Carsten Nielsen <heycarsten@gmail.com>
2011-01-17 14:01:23 -0500

    Added HTTP basic auth to /admin, probably not really needed but until I have something better I don't want it confusing people.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,20 @@
 class AdminController < ApplicationController
 
+  before_filter :authenticate
+
   layout 'admin'
+
+  protected
 
   def cacheable?
     false
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      LCBOAPI[:admin][:username] == username &&
+      LCBOAPI[:admin][:password] == password
+    end
   end
 
 end
