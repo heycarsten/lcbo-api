d7b80c8bea1452fd344a6f6ea2999143a0174346
Carsten Nielsen <heycarsten@gmail.com>
2011-01-19 00:59:40 -0500

    Please run the app as lcboapi.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,16 @@
 listen 3000
 worker_processes 4
-preload_app true
+
+user 'lcboapi'
 
 timeout 25
 
 working_directory '/home/lcboapi/unicorn.lcboapi.com/current'
-shared_path = '/home/lcboapi/unicorn.lcboapi.com/shared'
+shared_dir = '/home/lcboapi/unicorn.lcboapi.com/shared'
 
-pid         shared_path + '/pids/unicorn.pid'
-stderr_path shared_path + '/log/unicorn.stderr.log'
-stdout_path shared_path + '/log/unicorn.stdout.log'
+pid         shared_dir + '/pids/unicorn.pid'
+stderr_path shared_dir + '/log/unicorn.stderr.log'
+stdout_path shared_dir + '/log/unicorn.stdout.log'
 
 before_fork do |server, worker|
   # seamless deploy recipe courtesy of
@@ -22,7 +23,7 @@ before_fork do |server, worker|
   # note: see http://unicorn.bogomips.org/SIGNALS.html for an
   # even safer seamless restart setup (but requires enough RAM
   # to run two unicorn masters and two sets of workers)
-  old_pid = (shared_path + '/pids/unicorn.pid.oldbin')
+  old_pid = (shared_dir + '/pids/unicorn.pid.oldbin')
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill('QUIT', File.read(old_pid).to_i)
@@ -33,15 +34,5 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-  ##
-  # Unicorn master loads the app then forks off workers - because of the way
-  # Unix forking works, we need to make sure we aren't using any of the parent's
-  # sockets, e.g. db connection
-
   $memcache.reset
-
-  # Redis and Memcached would go here but their connections are established
-  # on demand, so the master never opens a socket
-
-  worker.user('lcboapi', 'lcboapi') if Process.euid == 0
 end
