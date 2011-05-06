bfdc2892f828334c628c250ecdda3b389d3d5987
Carsten Nielsen <heycarsten@gmail.com>
2011-05-06 01:58:51 -0400

    Don't use dead product names as source in the fuzzy name matcher.

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -13,7 +13,13 @@ Sequel::Model.plugin(:active_model)
 
 Fuzz.keyspace = Rails.env
 Fuzz.add_dictionary(:products,
-  :source => lambda { DB[:products].select(:name).all.map { |p| p[:name] } },
+  :source => lambda {
+    DB[:products].
+      select(:name).
+      filter(:is_dead => false).
+      all.
+      map { |p| p[:name] }
+  },
   :stop_words => %w[ woods ],
   :min_word_size => 5
 )
