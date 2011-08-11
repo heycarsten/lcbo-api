ce07f9e9bb7e57935b6d29fd14d1b23338816e02
Carsten Nielsen <heycarsten@gmail.com>
2011-08-11 12:28:08 -0400

    Ensure bundler is used for running cron jobs.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 set :output, '/home/lcboapi/logs/cron.log'
 
+job_type :rake, 'cd :path && RAILS_ENV=:environment bundle exec rake :task :output'
+
 every 1.day, :at => '3:43 AM' do
   rake 'cron'
 end
