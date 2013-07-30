b0517e006cba8b5069f14f0c4dd874f83289f357
Carsten Nielsen <heycarsten@gmail.com>
2013-07-30 13:30:22 -0400

    Updated deploy script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,40 @@
 default_run_options[:pty]   = true
 ssh_options[:forward_agent] = true
 
-set :rvm_ruby_string,      '1.9.3-p286'
-set :rvm_type,             :system
-set :repository,           'git@github.com:heycarsten/lcbo-api.git'
-set :scm,                  :git
-set :use_sudo,             false
-set :deploy_via,           :remote_cache
-set :copy_exclude,         %w[ .git ]
-set :keep_releases,        5
-set :user,                 'lcboapi'
-set :whenever_environment, defer { environment }
-set :rails_env,            defer { environment }
-set :bundle_flags,         '--deployment --quiet --binstubs'
-set :branch,               ENV['BRANCH'] || 'master'
-set :domain,               'lcboapi.com'
-set :environment,          'production'
-set :application,          'lcboapi'
-set :whenever_command,     defer { "#{release_path}/bin/whenever" }
-set :deploy_to,            '/home/lcboapi/lcboapi.com'
+set :rvm_ruby_string, '2.0.0'
+set :rvm_type,        :user
+set :repository,      'git@github.com:heycarsten/lcbo-api.git'
+set :scm,             :git
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :copy_exclude,    %w[ .git ]
+set :keep_releases,   5
+set :user,            'deploy'
+set :rails_env,       'production'
+set :bundle_flags,    '--deployment --quiet --binstubs'
 
-server 'lcboapi.com', :web, :app, :db, primary: true
+set :branch,      (ENV['branch'] || 'deployed')
+set :domain,      'lcboapi.com'
+set :environment, 'production'
+set :application, 'lcboapi'
+set :deploy_to,   '/sites/lcboapi.com'
+
+server '66.228.45.234', :web, :app, :db, primary: true
 
 after 'deploy:update_code', 'config:symlink'
 
 namespace :deploy do
   task :start do
-    sudo "start #{application}"
+    run "sudo start puma app=#{current_path}"
   end
 
   task :stop do
     on_rollback { start }
-    sudo "stop #{application}"
+    run "sudo stop puma app=#{current_path}"
   end
 
   task :restart do
-    sudo "restart #{application}"
+    run "sudo restart puma app=#{current_path}"
   end
 end
 
@@ -44,7 +43,6 @@ namespace :config do
   task :symlink do
     # note: capistrano automatically symlinks shared/log to current/log
     run "ln -nfs #{shared_path}/tmp #{release_path}/tmp"
-    run "ln -nfs #{shared_path}/sockets #{release_path}/tmp/sockets"
     run "ln -nfs #{shared_path}/config/*.yml #{release_path}/config/"
   end
 end
