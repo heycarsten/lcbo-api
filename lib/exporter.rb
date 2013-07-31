6dccba2e1f96eda31ca3be76975d3bae9ed7de0b
Carsten Nielsen <heycarsten@gmail.com>
2013-07-31 11:37:45 -0400

    Plz work.

--- lib/exporter.rb
+++ lib/exporter.rb
@@ -9,7 +9,8 @@ class Exporter
     @key = key
     @s3  = AWS::S3::S3Object
     @dir = File.join(Dir.tmpdir, 'lcboapi-tmp')
-    `mkdir -p #{@dir} && chmod 0777 #{@dir}`
+    `mkdir -p #{@dir}`
+    `chmod 0777 #{@dir}`
     @zip = File.join(@dir, Time.now.strftime('lcbo-%Y%m%d.zip'))
   end
 
@@ -60,11 +61,11 @@ class Exporter
   end
 
   def copy(table)
-    File.open(csv_file(table), 'w') do |file|
-      DB.copy_table("COPY #{table} (#{cols(table)}) TO STDOUT DELIMITER ',' CSV HEADER") do |line|
-        file.puts(line)
-      end
+    file = File.open(csv_file(table), 'w')
+    DB.copy_table("COPY #{table} (#{cols(table)}) TO STDOUT DELIMITER ',' CSV HEADER") do |line|
+      file.puts(line)
     end
+    file.close
   end
 
 end
