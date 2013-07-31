6865b3cddba789ab812275c80c36c92d13098eba
Carsten Nielsen <heycarsten@gmail.com>
2013-07-31 10:27:32 -0400

    Allow prefixed searching.

--- lib/sequel/extensions/texticle.rb
+++ lib/sequel/extensions/texticle.rb
@@ -2,15 +2,22 @@
 module Sequel
   class Dataset
 
+    DISALLOWED_TSQUERY_CHARACTERS = /['?\\:]/
+
     def search(colnames, rawquery, opts = {})
-      query = rawquery.to_s.gsub(/[^\w'\-]+/, ' ').gsub(/\s+/, ' ').strip
+      query = rawquery.to_s.
+        gsub(/[^\w'\-]+/, ' ').
+        gsub(DISALLOWED_TSQUERY_CHARACTERS, ' ').
+        gsub(/\s+/, ' ').
+        split.
+        join('+') + ':*'
       return self if '' == query.to_s
       lang = (opts[:lang] || 'simple')
       cols = Sequel.join(Array(colnames).map { |c| SQL::Function.new(:COALESCE, c, '') }, ' ')
-      filter(%{
+      where(%{
         to_tsvector(#{literal lang}, #{literal cols})
         @@
-        plainto_tsquery(#{literal lang}, ?)
+        to_tsquery(#{literal lang}, ?)
       }, query)
     end
 
