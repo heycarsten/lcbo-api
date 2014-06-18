2496c89d2c2376a427c5d0f0b35de15cfcc89e8b
Carsten Nielsen <heycarsten@gmail.com>
2014-06-18 15:57:23 -0400

    Tweak envs so that they actually work.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,27 +23,27 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = false
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
-  # config.assets.digest = true
+  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  # config.assets.version = '1.0'
+  config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
