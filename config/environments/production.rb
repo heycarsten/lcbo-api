234b434eb9d1d1025a4536db248668655b34b503
Carsten Nielsen <heycarsten@gmail.com>
2014-06-18 16:36:47 -0400

    Use SMTP in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'lcboapi.com' }
+  config.action_mailer.smtp_settings = Rails.application.secrets.smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
