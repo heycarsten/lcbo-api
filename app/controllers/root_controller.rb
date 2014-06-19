6a19bc47f76180733a6f111e7c0f64576ec1eb90
Carsten Nielsen <heycarsten@gmail.com>
2014-06-18 22:27:29 -0400

    Cache documents data in memory.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -38,13 +38,7 @@ class RootController < ApplicationController
   protected
 
   def load_documents
-    docs_path = (Rails.root + 'db' + 'documents.yml').to_s
-    raw_yaml  = File.read(docs_path)
-    yaml      = ERB.new(raw_yaml).result
-    @sections = YAML.load(yaml)
-    @sections.values.each do |section|
-      section.each { |doc| doc[:slug] = (doc[:menu] || doc[:title]).parameterize }
-    end
-    @documents = @sections.values.flatten
+    @sections  = SECTIONS
+    @documents = DOCUMENTS
   end
 end
