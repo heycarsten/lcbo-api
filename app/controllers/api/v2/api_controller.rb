8c8ebb586168b408c069f60c83bd99ec525584b7
Carsten Nielsen <heycarsten@gmail.com>
2014-07-06 23:51:02 -0400

    Implements rate limiting.

---
+++ app/controllers/api/v2/api_controller.rb
@@ -0,0 +1,56 @@
+class API::V2::APIController < APIController
+  VERSION  = 2
+  RATE_MAX = 1800
+
+  before_filter :rate_limit!
+  before_filter :authenticate!
+
+  protected
+
+  def api_version
+    VERSION
+  end
+
+  def api_key
+    headers['X-API-Key'] || params[:api_key]
+  end
+
+  def auth_token
+    headers['X-Auth-Token']
+  end
+
+  def rate_limit!
+    count_key = "#{api_key || request.ip}:ratelimit:count"
+    max_key   = "#{api_key}:ratelimit:max"
+    max       = ($redis.get(max_key) || RATE_MAX).to_i
+    count     = $redis.incr(count_key).to_i
+
+    if count == 1
+      $redis.expire(count_key, 1.hour)
+    end
+
+    ttl = $redis.ttl(count_key).to_i + 1
+
+    response.headers['X-Rate-Limit-Max']   = max
+    response.headers['X-Rate-Limit-Count'] = count
+    response.headers['X-Rate-Limit-Reset'] = ttl
+
+    if count > max
+      render json: {
+        message: I18n.t('rate_limited', max: max, ttl: ttl)
+      }, status: 403
+
+      return false
+    end
+
+    true
+  end
+
+  def authenticate!
+    render json: {
+      message: I18n.t('unauthorized')
+    }, status: 401
+
+    false
+  end
+end
