e6901224bd4d904a81126a027c3dc952dbe38d29
Carsten Nielsen <heycarsten@gmail.com>
2014-07-09 21:14:00 -0400

    Twerk dat config bb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,12 +27,12 @@ RSpec.configure do |config|
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
-  # if config.files_to_run.one?
-  #   # Use the documentation formatter for detailed output,
-  #   # unless a formatter has already been configured
-  #   # (e.g. via a command-line flag).
-  #   config.default_formatter = 'doc'
-  # end
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  end
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
@@ -54,23 +54,23 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
-  # config.expect_with :rspec do |expectations|
-  #   # Enable only the newer, non-monkey-patching expect syntax.
-  #   # For more details, see:
-  #   #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
-  #   expectations.syntax = :expect
-  # end
+  config.expect_with :rspec do |expectations|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    # For more details, see:
+    #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
+    expectations.syntax = :expect
+  end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
-  # config.mock_with :rspec do |mocks|
-  #   # Enable only the newer, non-monkey-patching expect syntax.
-  #   # For more details, see:
-  #   #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
-  #   mocks.syntax = :expect
+  config.mock_with :rspec do |mocks|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    # For more details, see:
+    #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+    mocks.syntax = :expect
 
-  #   # Prevents you from mocking or stubbing a method that does not exist on
-  #   # a real object. This is generally recommended.
-  #   mocks.verify_partial_doubles = true
-  # end
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended.
+    mocks.verify_partial_doubles = true
+  end
 end
