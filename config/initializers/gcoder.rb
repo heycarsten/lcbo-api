aac6a31e3bb2c83227136480b13cf81bd528d388
Carsten Nielsen <heycarsten@gmail.com>
2014-10-31 10:24:36 -0400

    Don't do fancy shit.

--- config/initializers/gcoder.rb
+++ config/initializers/gcoder.rb
@@ -1,9 +1,8 @@
 GEO = GCoder.connect \
-  storage:        :redis,
+  storage: :redis,
   storage_config: {
-    url:          Rails.application.secrets.redis,
-    key_ttl:      86400 # 24 hours
+    url: Rails.application.secrets.redis
   },
-  bounds:         [[50.09, -94.88], [41.87, -74.16]], # Ontario: The Populated Parts
-  region:         :ca,
-  append:         ', Ontario, Canada'
+  bounds: [[50.09, -94.88], [41.87, -74.16]], # Ontario: The Populated Parts
+  region: :ca,
+  append: ', Ontario, Canada'
