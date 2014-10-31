0830fc8a3e84d0c0b6a63b55487245384ab7fd57
Carsten Nielsen <heycarsten@gmail.com>
2014-10-31 10:44:22 -0400

    Expire cached geocoded values after 24h.

--- config/initializers/gcoder.rb
+++ config/initializers/gcoder.rb
@@ -1,7 +1,8 @@
 GEO = GCoder.connect \
   storage: :redis,
   storage_config: {
-    url: Rails.application.secrets.redis
+    url:     Rails.application.secrets.redis,
+    key_ttl: 86400 # 24 hours
   },
   bounds: [[50.09, -94.88], [41.87, -74.16]], # Ontario: The Populated Parts
   region: :ca,
