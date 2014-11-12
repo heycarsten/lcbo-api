a365179419c35d4e529daee4bfe38ea56f8a775f
Carsten Nielsen <heycarsten@gmail.com>
2014-11-11 21:02:36 -0500

    Use HTTPS in manager shit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,11 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'lcboapi.com' }
+  config.action_mailer.default_url_options = {
+    host: 'lcboapi.com',
+    protocol: 'https'
+  }
+
   config.action_mailer.smtp_settings = Rails.application.secrets.smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
