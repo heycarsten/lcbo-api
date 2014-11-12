d6c879cd80d0d02d66c90ff8a6af52e4401f6139
Carsten Nielsen <heycarsten@gmail.com>
2014-11-11 22:20:59 -0500

    Feck.

--- manager/app/routes/dashboard/keys.js
+++ manager/app/routes/dashboard/keys.js
@@ -2,11 +2,6 @@ import Em from 'ember';
 
 export default Em.Route.extend({
   model: function() {
-    return this.store.find('key', { page: 1 });
-  },
-
-  afterModel: function() {
-    var meta = this.store.metadataFor('key');
-    this.controllerFor('keys').set('page', meta.pagination.current_page);
+    return this.store.find('key');
   }
 });
