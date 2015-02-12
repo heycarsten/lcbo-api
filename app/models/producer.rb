32b1511f3efcb73e70c5abe37aa323fd05438aec
Carsten Nielsen <heycarsten@gmail.com>
2015-02-12 12:24:53 -0500

    Adds ability to identify dead producers.

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -3,6 +3,28 @@ class Producer < ActiveRecord::Base
 
   has_many :products
 
+  def self.mark_dead!(&each)
+    Producer.find_each do |producer|
+      each.(producer) if each
+
+      all_count = producer.products.count
+
+      if all_count == 0
+        producer.update(is_dead: true)
+        next
+      end
+
+      if all_count == producer.products.where(is_dead: true).count
+        producer.update(is_dead: true)
+        next
+      end
+
+      if producer.is_dead
+        producer.update(is_dead: false)
+      end
+    end
+  end
+
   def self.fetch_by_lcbo_name(lcbo_name)
     lcbo_ref = lcbo_name.parameterize
 
