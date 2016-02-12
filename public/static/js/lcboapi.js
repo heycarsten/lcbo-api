ea30101fe8c2cf1d4792c9ab75ac854e72d5b7f3
Carsten Nielsen <heycarsten@gmail.com>
2016-02-12 11:19:05 -0500

    Rename static assets to "static" to avoid collision with asset pipeline.

---
+++ public/static/js/lcboapi.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require ./vendor/bs.scrollspy
+//= require ./vendor/bs.affix
+//= require prism/prism.js
+//= require react-with-addons
+//= require_tree ./components
+//= require_self
